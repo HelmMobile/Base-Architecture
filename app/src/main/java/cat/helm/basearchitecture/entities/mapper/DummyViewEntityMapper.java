db3667d6b21cd8a70e732fd6550739022e81c2eb
Borja Gonzalez de Pablo <borja@helm.cat>
2016-02-25 16:03:55 +0100

    renamed ViewEntityMapper

---
+++ app/src/main/java/cat/helm/basearchitecture/entities/mapper/DummyViewEntityMapper.java
@@ -0,0 +1,25 @@
+/*
+Created by Helm  25/1/16.
+*/
+
+
+package cat.helm.basearchitecture.entities.mapper;
+import cat.helm.basearchitecture.entities.DummyViewEntity;
+import cat.helm.basearchitecture.model.Dummy;
+
+import javax.inject.Inject;
+
+public class DummyViewEntityMapper extends Mapper<DummyViewEntity, Dummy> {
+
+    @Inject
+    public DummyViewEntityMapper() {
+        //DIP purposes
+    }
+
+    @Override
+    public DummyViewEntity map(Dummy entity) {
+        return new DummyViewEntity(entity.getString());
+    }
+
+
+}
